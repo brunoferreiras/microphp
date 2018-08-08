fc76d5ea6609c2420f59e54aa8944055ad758257
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-08-08 14:15:03 -0300

    Finish router collection

--- app.php
+++ app.php
@@ -3,3 +3,7 @@
 require __DIR__ . '/vendor/autoload.php';
 
 $router = new BrunoFerreiras\Router\Router;
+
+$router->get('/hello', function() {
+    return 'Hello!';
+});
--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,8 +4,25 @@ namespace BrunoFerreiras\Router;
 
 class Router
 {
+    private $collection;
+
     public function __construct()
     {
-        echo "It's working!";
+        $this->collection = new RouterCollection;
+    }
+
+    public function get($path, $callback)
+    {
+        $this->request('GET', $path, $callback);
+    }
+
+    public function post($path, $callback)
+    {
+        $this->request('POST', $path, $callback);
+    }
+
+    public function request($method, $path, $callback)
+    {
+        $this->collection->add($method, $path, $callback);
     }
 }
