161bdfb9d96c45f1d2da82109cb75b32ec0b7356
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-08-08 13:52:09 -0300

    Create the project structure of micro framework in php

---
+++ src/Router/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BrunoFerreiras\Router;
+
+class Router
+{
+    public function __construct()
+    {
+        echo "It's working!";
+    }
+}
---
+++ src/Router/RouterCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BrunoFerreiras\Router;
+
+use Illuminate\Support\Collection;
+
+class RouterCollection
+{
+    protected $collection = [];
+
+    public function add(string $method, string $path, $callback)
+    {
+        if (!isset($this->collection[$method])) {
+            $this->collection[$method] = new Collection;
+        }
+        $this->collection[$method]->put($path, $callback);
+    }
+}
