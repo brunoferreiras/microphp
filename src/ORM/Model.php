e8468a010c8350b06b428124096bf2e49b7045ad
Bruno Ferreira <fs.brunoferreira@gmail.com>
2018-08-16 13:47:11 -0300

    Create methods of persistence in model

--- src/ORM/Model.php
+++ src/ORM/Model.php
@@ -6,33 +6,46 @@ use BrunoFerreiras\ORM\Drivers\DriverStrategy;
 
 class Model
 {
+    protected $driver;
+
     public function setDrive(DriverStrategy $driver)
     {
-        return $driver;
+        $this->driver;
+        return $this;
     }
 
     protected function getDriver()
     {
-
+        return $this->driver;
     }
 
     public function save()
     {
-
+        $this->getDriver()
+            ->save($this)
+            ->exec();
     }
 
     public function findAll(array $conditions = [])
     {
-
+        return $this->getDriver()
+            ->select($conditions)
+            ->exec()
+            ->all();
     }
 
     public function findFirst($id)
     {
-
+        return $this->getDriver()
+            ->select(['id' => $id])
+            ->exec()
+            ->first();
     }
 
     public function delete()
     {
-
+        $this->getDriver()
+            ->delete(['id' => $this->id])
+            ->exec();
     }
 }
